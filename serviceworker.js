14d9c570a7b145f527a6d06c3da8a95d68f9cb46
Peter Holmes <peter@holmes.net.au>
2025-06-08 16:15:25 +1000

    Updated for the android app

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,26 @@
-self.addEventListener("fetch", function(event) 
-{
-  console.log("start server worker")
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('app-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/app-images/appicon-144.png',
+                '/app-images/appicon-192.png',
+                '/app-images/appicon-256.png',
+                '/app-images/appicon-512.png',
+                '/index.html',
+                '/manifest.json',
+                '/serviceworker.js',
+                '/cleancab.png',
+                '/styleph.css'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
